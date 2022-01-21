7b7c524ecdc36edf214ef42fa2ddcbc817970571
stefanvesely <baws.steff@gmail.com>
2022-01-21 09:33:50 +0200

    Initial commit

---
+++ HiddenVilla_Server/Model/BlazorRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HiddenVilla_Server.Model
+{
+    public class BlazorRoom
+    {
+        public int Id { get; set; }
+        public string RoomName { get; set; }
+        public decimal Price { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<RoomProperites> RoomProps { get; set; }
+        
+    }
+}
